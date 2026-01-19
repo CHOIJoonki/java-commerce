04aa19464a69b55d6964bb4f2985a8d1f240109e
CHOIJoonki <scarmic@naver.com>
2026-01-20 01:18:02 +0900

    feat(step1): Product 클래스 및 상품 목록 출력 기능 구현

---
+++ src/step1/Product.java
@@ -0,0 +1,16 @@
+package step1;
+
+
+public class Product {
+    String name;
+    int price;
+    String description;
+    int stock;
+
+    public Product(String name, int price, String description, int stock) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+    }
+}
