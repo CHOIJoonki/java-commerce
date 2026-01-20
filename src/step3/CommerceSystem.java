7a6680f1c264ab8eec9d8034ff1d794c4dce5b95
CHOIJoonki <scarmic@naver.com>
2026-01-20 12:40:30 +0900

    -CommerceSystem 수정, Category관련 속성, 기능 추가

--- src/step3/CommerceSystem.java
+++ src/step3/CommerceSystem.java
@@ -1,6 +1,5 @@
 package step3;
 
-
 import java.text.DecimalFormat;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -8,24 +7,59 @@ import java.util.Scanner;
 
 
 public class CommerceSystem {
-    List<Product> products;
+    List<Category> categories;
     Scanner scanner;
     DecimalFormat formatter;
 
-    public CommerceSystem(List<Product> products) {
-        this.products = products;
+    public CommerceSystem(List<Category> categories) {
+        this.categories = categories;
         this.scanner = new Scanner(System.in);
         this.formatter = new DecimalFormat("#,###");
     }
 
     public void start() {
         while (true) {
-            printProductList();
+            printMainMenu();
+
+            int choice = getValidInput(0, categories.size());
+
+            if (choice == 0) {
+                System.out.println("커머스 플랫폼을 종료합니다.");
+                break;
+            } else if (choice >= 1 && choice <= categories.size()) {
+                Category selectedCategory = categories.get(choice - 1);
+                showCategoryMenu(selectedCategory);
+            }
+        }
+        scanner.close();
+    }
+
+    private void printMainMenu() {
+        System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
+        for (int i = 0; i < categories.size(); i++) {
+            System.out.println((i + 1) + ". " + categories.get(i).getCategoryName());
+        }
+        System.out.println("0. 종료      | 프로그램 종료");
+    }
+
+    private void showCategoryMenu(Category category) {
+        while (true) {
+            System.out.println();
+            System.out.println("[ " + category.getCategoryName() + " 카테고리 ]");
+
+            List<Product> products = category.getProducts();
+            for (int i = 0; i < products.size(); i++) {
+                Product product = products.get(i);
+                String formattedPrice = String.format("%,10d", product.price);
+                System.out.println((i + 1) + ". " + String.format("%-14s", product.name)
+                        + " |" + formattedPrice + "원 | " + product.description);
+            }
+            System.out.println("0. 뒤로가기");
 
             int choice = getValidInput(0, products.size());
 
             if (choice == 0) {
-                System.out.println("커머스 플랫폼을 종료합니다.");
+                System.out.println();
                 break;
             } else if (choice >= 1 && choice <= products.size()) {
                 Product selected = products.get(choice - 1);
@@ -33,20 +67,7 @@ public class CommerceSystem {
                         + formatter.format(selected.price) + "원 | "
                         + selected.description + " | 재고: " + selected.stock + "개");
             }
-            System.out.println();
         }
-        scanner.close();
-    }
-
-    private void printProductList() {
-        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-        for (int i = 0; i < products.size(); i++) {
-            Product product = products.get(i);
-            String formattedPrice = String.format("%,10d", product.price);
-            System.out.println((i + 1) + ". " + String.format("%-14s", product.name)
-                    + " |" + formattedPrice + "원 | " + product.description);
-        }
-        System.out.println("0. 종료           | 프로그램 종료");
     }
 
     private int getValidInput(int min, int max) {
