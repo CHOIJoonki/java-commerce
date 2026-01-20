d4cd358c4a112ec45887bcc8f3dc0d779b52a729
CHOIJoonki <scarmic@naver.com>
2026-01-20 19:47:56 +0900

    init step4

---
+++ src/step4/Category.java
@@ -0,0 +1,27 @@
+package step4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Category {
+    String categoryName;
+    List<Product> products;
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
---
+++ src/step4/Customer.java
@@ -0,0 +1,13 @@
+package step4;
+
+public class Customer {
+    String name;
+    String email;
+    String grade;
+
+    public Customer(String name, String email, String grade) {
+        this.name = name;
+        this.email = email;
+        this.grade = grade;
+    }
+}
