ed9850962d7bfad22f271587c49e678897aa6667
CHOIJoonki <scarmic@naver.com>
2026-01-20 19:59:06 +0900

    step4 CustomerClass add setter, encapculation

--- src/step4/Customer.java
+++ src/step4/Customer.java
@@ -1,13 +1,38 @@
 package step4;
 
 public class Customer {
-    String name;
-    String email;
-    String grade;
+
+    private String name;
+    private String email;
+    private String grade;
 
     public Customer(String name, String email, String grade) {
         this.name = name;
         this.email = email;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGrade(String grade) {
         this.grade = grade;
     }
 }
